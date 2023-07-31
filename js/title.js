10694dc6e01e6b42c51a1ac5fce9dca01eae976a
robust-vase <robustvase@qq.com>
2023-07-31 12:48:50 +0800

    Site updated: 2023-07-31 12:48:48

--- js/title.js
+++ js/title.js
@@ -4,7 +4,7 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '花瓶不想离开🤧';
+    document.title = '🤧不要你离开～';
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
